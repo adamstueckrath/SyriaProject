7ad44e4916d7023d456e294a7ebcc4cc7a76bcd1
Adam Stueckrath <stueckrath.adam@gmail.com>
2018-09-23 13:49:25 -0500

    Add .gitignore file and updates to twitter_web_scraper

--- web_scrapers/twitter_web_scraper.py
+++ web_scrapers/twitter_web_scraper.py
@@ -1,16 +1,28 @@
 import twint
 
+def main():
+	
+	c = twint.Config()
+	
+	# coordinates of Aleppo, Syria with a 4km radius 
+	c.Geo = "36.2012400, 37.1611700, 4km"
+	
+	# get all tweets from august to septmeber at the locaiton
+	c.Since = "2018-8-1"
+	c.Until = "2018-9-1"
 
-c = twint.Config()
-c.Geo = "36.2012400,37.1611700,5km"
-c.Since = "2017-1-1"
-c.Lang = "en"
-# Custom output format
-c.Store_csv = True
-c.Custom_csv = ["id", "date", "user_id", "username", "tweet", "hashtags", "location"]
-c.Output = "twitter.csv"
+	# translate arabic to english
+	c.Lang = "en"
 
-twint.run.Search(c)
+	# custom output format
+	c.Store_csv = True
+
+	# custom twitter fields to capture
+	c.Custom_csv = ["id", "date", "user_id", "username", "tweet", "hashtags", "location"]
+	c.Output = "twitter.csv"
+
+	# write to file
+	twint.run.Search(c)
 
 
 if __name__ == '__main__':
